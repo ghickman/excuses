a20cf6f5f67459ea3f9973162e30741a7f0fe8d1
George Hickman <george@ghickman.co.uk>
2012-01-14 09:58:03 +0000

    Remove noise in the admin

--- excuses/core/admin.py
+++ excuses/core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from django.contrib.sites.models import Site
 
 from excuses.core.models import Excuse
 
 
 admin.site.register(Excuse)
+admin.site.unregister(Group)
+admin.site.unregister(User)
+admin.site.unregister(Site)
 
