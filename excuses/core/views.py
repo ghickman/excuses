37d35c9ed1a915cd4c110be031ac05cf4160d274
George Hickman <george@ghickman.co.uk>
2012-01-14 10:03:43 +0000

    Roll my own random record retriever

--- excuses/core/views.py
+++ excuses/core/views.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from django.views.generic import DetailView
 
 from excuses.core.models import Excuse
@@ -5,5 +7,7 @@ from excuses.core.models import Excuse
 
 class Home(DetailView):
     def get_object(self, queryset=None):
-        return Excuse.objects.order_by('?')[0]
+        excuses = Excuse.objects.all()
+        excuse_pks = [pk[0] for pk in excuses.values_list('pk')]
+        return excuses.get(pk=choice(excuse_pks))
 
