271290d51c4d50981d276a367a07c4fb805cbbcb
George Hickman <george@ghickman.co.uk>
2012-03-15 10:14:53 -0700

    Make sure an empty db doesn't mess with the site

--- excuses/core/views.py
+++ excuses/core/views.py
@@ -8,6 +8,8 @@ from excuses.core.models import Excuse
 class Home(DetailView):
     def get_object(self, queryset=None):
         excuses = Excuse.objects.all()
+        if not excuses:
+            return Excuse(excuse='No excuses yet!')
         excuse_pks = [pk[0] for pk in excuses.values_list('pk')]
         return excuses.get(pk=choice(excuse_pks))
 
